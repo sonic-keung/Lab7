4956be3751a4529716ed1d7e3473f7478cfbb431
Sonic Keung <skeung3@my.bcit.ca>
2018-11-09 18:58:36 -0800

    finished lab

--- Course.cpp
+++ Course.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include "Course.hpp"
 
 using namespace std;
@@ -10,22 +11,50 @@ Course::Course (const Course & m) : title (m.title), day (m.day), start_time (m.
 }
 
 Course & Course::operator =(const Course & m) {
-    //TODO implement the operator
+    this->title = m.title;
+    this->day = m.day;
+    this->start_time = m.start_time;
+    this->finish_time = m.finish_time;
     return *this;
 }
 bool Course::operator == (const Course & m) const {
-    //TODO implement the operator
+    if (this->day == m.day) {
+        return true;
+    } else if (this->start_time == m.start_time && this->finish_time == m.finish_time) {
+        return true;
+    }
+
     return false;
 }
 
 bool Course::operator < (const Course & m) const
 {
-    //TODO implement the operator
+    if (this->day < m.day) {
+        return true;
+    }
     return false;
 }
 
 ostream & operator << (ostream &os, const Course & m)
 {
-    //TODO implement the operator
+    char day;
+    switch(m.day) {
+        case 0:
+            day = 'M';
+            break;
+        case 1:
+            day = 'T';
+            break;
+        case 2:
+            day = 'W';
+            break;
+        case 3:
+            day = 'R';
+            break;
+        case 4:
+            day = 'F';
+            break;
+    }
+    os << m.title << setw(4) << day << setw(8) << m.start_time << setw(8) << m.finish_time << endl;
     return os;
 }
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <map>
 #include "Course.hpp"
 
 using namespace std;
@@ -15,19 +16,67 @@ using namespace std;
  * print schedule - outputs course schedule to the screen
  * @param - add any new parameters you need
  */
-void printSchedule()
+void printSchedule(vector<Course> &path)
 {
-    //TODO implement your print using the copy algorithm, 2 iterators, and an ostream_iterator
+    for(auto i = path.begin(); i != prev(path.end()); ++i) {
+        for(auto j = next(i); j != path.end(); ++j) {
+            if((j->start_time >= i->start_time && j->start_time <= i->finish_time && j->day == i->day)
+               || (j->finish_time >= i->start_time && j->finish_time <= i->finish_time && j->day == i->day)) {
+                cout << "CONFLICT:" << endl;
+                cout << *i;
+                cout << *j << endl;
+            }
+        }
+    }
+
+    //Prints the entire course list using o_stream iterator
+    ostream_iterator<Course> out_it (std::cout,"");
+    copy(path.begin(), path.end(), out_it);
 }
 
 int main () {
-    //TODO read from courses.txt
-    //TODO store data in an STL container
-    //TODO sort your STL container with the sort algorithm
-    //TODO implement code to determine schedule conflicts
-    //TODO print out schedule conflicts
-    //TODO print out schedule
-    cout << "Find the TODOs in the code and implement them." << endl;
-    cout << "Add/modify any functions/code you need to complete your task." << endl;
+    vector<Course> myVector;
+    ifstream file;
+    file.open("../courses.txt");
+    if (!file) {
+        cout << "cannot open the file" << endl;
+    }
+    string readLine;
+    while(getline(file, readLine)) {
+        istringstream iss(readLine);
+        string courseName;
+        Course::dayOfWeek day;
+        char tempDay;
+        unsigned int start_time;
+        unsigned int end_time;
+        iss >> courseName;
+        iss >> tempDay;
+        switch(tempDay) {
+            case 'M':
+                day = Course::MON;
+                break;
+            case 'T':
+                day = Course::TUE;
+                break;
+            case 'W':
+                day = Course::WED;
+                break;
+            case 'R':
+                day = Course::THUR;
+                break;
+            case 'F':
+                day = Course::FRI;
+                break;
+            default:
+                break;
+        }
+        iss >> start_time;
+        iss >> end_time;
+        Course course(courseName, day, start_time, end_time);
+        myVector.push_back(course);
+    }
+    //sort the stl container
+    sort(myVector.begin(), myVector.end());
+    printSchedule(myVector);
     return 0;
 }
